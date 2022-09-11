e8c6fab70985cb74b1c90b12a00f53e3cdeb4d24
Alex_sz1 <szolnoki.alex699@gmail.com>
2022-09-11 16:32:43 +0200

    Commit
    Working chord adder, play mode on/off,

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.firstapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Switch;
 
 import com.example.firstapp.ui.chordadder.AddChord;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,8 +30,58 @@ import com.example.firstapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    private int buttonId;
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private ActivityResultLauncher<Intent> chordNameResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == 200){
+                        switch (buttonId){
+                            case R.id.button1 :
+                                button = (Button) findViewById(R.id.button1);
+                                break;
+                            case R.id.button2 :
+                                button = (Button) findViewById(R.id.button2);
+                                break;
+                            case R.id.button3 :
+                                button = (Button) findViewById(R.id.button3);
+                                break;
+                            case R.id.button4 :
+                                button = (Button) findViewById(R.id.button4);
+                                break;
+                            case R.id.button5 :
+                                button = (Button) findViewById(R.id.button5);
+                                break;
+                            case R.id.button6 :
+                                button = (Button) findViewById(R.id.button6);
+                                break;
+                            case R.id.button7 :
+                                button = (Button) findViewById(R.id.button7);
+                                break;
+                            case R.id.button8 :
+                                button = (Button) findViewById(R.id.button8);
+                                break;
+
+                        }
+
+                        Intent intent = result.getData();
+                        String data = "";
+
+                         if (intent != null){
+                             data = intent.getStringExtra("result");
+                         }
+
+                         button.setText(data);
+
+                    }
+                }
+            }
+    );
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +102,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
     }
 
     @Override
@@ -57,9 +120,33 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    public void handleClick(View view){
-        startActivity(new Intent(MainActivity.this, AddChord.class));
+    public void handleClick(View v){
+        Switch modeSwitch = (Switch) findViewById(R.id.switch_mode);
+
+        Boolean play = modeSwitch.isChecked();
+
+        if(!play){
+            Intent intent = new Intent(MainActivity.this, AddChord.class);
+            buttonId = v.getId();
+            chordNameResult.launch(intent);
+        }else{
+            Button b = (Button)v;
+            String buttonText = b.getText().toString();
+
+            System.out.println(buttonText);
+        }
+
+
+
     }
+
+
+
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/example/firstapp/ui/chordadder/AddChord.java
+++ app/src/main/java/com/example/firstapp/ui/chordadder/AddChord.java
@@ -1,16 +1,27 @@
 package com.example.firstapp.ui.chordadder;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.widget.ButtonBarLayout;
+
+import com.example.firstapp.MainActivity;
 import com.example.firstapp.R;
+import com.google.android.material.navigation.NavigationView;
 
 
-public class AddChord extends AppCompatActivity {
+public class AddChord extends Activity {
+
+    public static String buttonname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -20,11 +31,59 @@ public class AddChord extends AppCompatActivity {
         Spinner chords=findViewById(R.id.spinner_chords);
 
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.chords, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-
         chords.setAdapter(adapter);
+
+        Spinner chordtype=findViewById(R.id.spinner_chordtype);
+
+        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this, R.array.chordtype, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        chordtype.setAdapter(adapter2);
+
     }
+
+
+    public void handleCancel(View v){
+        this.finish();
+    }
+
+    public void handleAddChord(View v){
+        Spinner chords=findViewById(R.id.spinner_chords);
+        Spinner chordtype=findViewById(R.id.spinner_chordtype);
+
+        String chord = chords.getSelectedItem().toString();
+        String type = chordtype.getSelectedItem().toString();
+
+
+
+        buttonname = chord + " " + type.toLowerCase();
+
+        Intent intent = new Intent();
+        intent.putExtra("result", buttonname);
+        setResult(200,intent);
+
+        finish();
+    }
+
+    public void handlePlayChord(View view){
+        Spinner chords=findViewById(R.id.spinner_chords);
+        Spinner chordtype=findViewById(R.id.spinner_chordtype);
+
+        String chord = chords.getSelectedItem().toString();
+        String type = chordtype.getSelectedItem().toString();
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 
 
 
